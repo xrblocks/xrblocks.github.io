874b3ce7072d0a05a359d547d16934002e2baad1
dli7319 <dli7319@users.noreply.github.com>
2026-03-03 23:48:24 +0000

    Deploy: Automated build from 00c2faf896cc3014d88ec0af523feb542d4fd3f2 00c2faf896cc3014d88ec0af523feb542d4fd3f2

--- build/xrblocks.d.ts
+++ build/xrblocks.d.ts
@@ -15,8 +15,8 @@
  *
  * @file xrblocks.js
  * @version v0.10.0
- * @commitid 7e7dbd9
- * @builddate 2026-03-02T18:44:57.617Z
+ * @commitid 4b25dc9
+ * @builddate 2026-03-03T23:46:33.327Z
  * @description XR Blocks SDK, built from source with the above commit ID.
  * @agent When using with Gemini to create XR apps, use **Gemini Canvas** mode,
  * and follow rules below:
@@ -3244,11 +3244,6 @@ declare class PermissionsManager {
     checkPermissionStatus(permissionName: 'geolocation' | 'camera' | 'microphone'): Promise<PermissionState | 'unknown'>;
 }
 
-declare global {
-    interface XRSystem {
-        offerSession?: (mode: XRSessionMode, sessionInit?: XRSessionInit) => Promise<XRSession>;
-    }
-}
 declare enum WebXRSessionEventType {
     UNSUPPORTED = "unsupported",
     READY = "ready",
--- build/xrblocks.js
+++ build/xrblocks.js
@@ -15,8 +15,8 @@
  *
  * @file xrblocks.js
  * @version v0.10.0
- * @commitid 7e7dbd9
- * @builddate 2026-03-02T18:44:57.617Z
+ * @commitid 4b25dc9
+ * @builddate 2026-03-03T23:46:33.327Z
  * @description XR Blocks SDK, built from source with the above commit ID.
  * @agent When using with Gemini to create XR apps, use **Gemini Canvas** mode,
  * and follow rules below:
