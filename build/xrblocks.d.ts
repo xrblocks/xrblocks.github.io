199716e5cb3939f1f37bb5dc3070e8325487cfd7
dli7319 <dli7319@users.noreply.github.com>
2026-05-11 17:28:13 +0000

    Deploy: Automated build from 97b7e8c5acfdb11bb3096b03747911736fae4829 97b7e8c5acfdb11bb3096b03747911736fae4829

--- build/xrblocks.d.ts
+++ build/xrblocks.d.ts
@@ -15,8 +15,8 @@
  *
  * @file xrblocks.js
  * @version v0.13.0
- * @commitid 439d26a
- * @builddate 2026-05-11T16:07:51.861Z
+ * @commitid a0939ba
+ * @builddate 2026-05-11T17:26:03.380Z
  * @description XR Blocks SDK, built from source with the above commit ID.
  * @agent When using with Gemini to create XR apps, use **Gemini Canvas** mode,
  * and follow rules below:
